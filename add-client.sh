3676cd2807a9e18f1fb219347ad89e96bcf1685a
David Gross <david.gross@daggerweb.org>
2020-03-13 09:09:21 -0600

    clean up server pub key

--- add-client.sh
+++ add-client.sh
@@ -10,7 +10,8 @@ else
 	key=$(cat clients/$1/$1.priv) 
 	ip="10.8.0."$(expr $(cat last-ip.txt | tr "." " " | awk '{print $4}') + 1)
 	FQDN=$(hostname -f)
-	cat wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_ADDRESS:|'"$FQDN"'|' > clients/$1/wg0.conf
+  SERVER_PUB_KEY=$(cat /etc/wireguard/server_public_key)
+  cat wg0-client.example.conf | sed -e 's/:CLIENT_IP:/'"$ip"'/' | sed -e 's|:CLIENT_KEY:|'"$key"'|' | sed -e 's|:SERVER_PUB_KEY:|'"$SERVER_PUB_KEY"'|' | sed -e 's|:SERVER_ADDRESS:|'"$FQDN"'|' > clients/$1/wg0.conf
 	echo $ip > last-ip.txt
 	cp SETUP.txt clients/$1/SETUP.txt
 	tar czvf clients/$1.tar.gz clients/$1
